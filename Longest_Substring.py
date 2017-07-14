a814133e0acb1dbc1e0370b9807ba1518313dbf2
Nov4213 <wenborao@163.com>
2017-07-15 00:42:01 +0800

    num#3

---
+++ Longest_Substring.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i = 0
+        lens = 0
+        smap = {}
+        for j in range(len(s)):
+            if s[j] in smap:
+                i = max(smap[s[j]], i)
+            lens = max(lens, j-i+1)
+            smap[s[j]] = j+1
+        return lens
+
+
+
+def main():
+    s = "aaaaaaaa"
+    solu = Solution()
+    result = solu.lengthOfLongestSubstring(s)
+    print(result)
+    # assert result == 2
+
+if __name__ == '__main__':
+    main()
